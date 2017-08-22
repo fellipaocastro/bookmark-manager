6c79cc31da6f1fb8a475ae4055163cda7f91c3fd
Fellipe Castro <contact@fellipecastro.com>
2017-08-22 19:52:20 -0300

    Add favicon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,10 +6,21 @@ export class Bookmark {
   url: string;
 }
 
+const BOOKMARKS: Bookmark[] = [
+  { id: 11, name: 'Google 11', url: 'http://www.google.com' },
+  { id: 12, name: 'Google 12', url: 'http://www.google.com' },
+  { id: 13, name: 'Google 13', url: 'http://www.google.com' },
+  { id: 14, name: 'Google 14', url: 'http://www.google.com' },
+  { id: 15, name: 'Google 15', url: 'http://www.google.com' },
+  { id: 16, name: 'Google 16', url: 'http://www.google.com' },
+  { id: 17, name: 'Google 17', url: 'http://www.google.com' },
+  { id: 18, name: 'Google 18', url: 'http://www.google.com' },
+  { id: 19, name: 'Google 19', url: 'http://www.google.com' },
+  { id: 20, name: 'Google 20', url: 'http://www.google.com' }
+];
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   template: `
     <h1>{{title}}</h1>
     <h2>My Bookmarks</h2>
@@ -83,22 +94,10 @@ export class Bookmark {
 })
 export class AppComponent {
   title = 'Bookmark Manager';
-  selectedBookmark: Bookmark;
   bookmarks = BOOKMARKS;
+  selectedBookmark: Bookmark;
+
   onSelect(bookmark: Bookmark): void {
     this.selectedBookmark = bookmark;
   }
 }
-
-const BOOKMARKS: Bookmark[] = [
-  { id: 11, name: 'Google 11', url: 'http://www.google.com' },
-  { id: 12, name: 'Google 12', url: 'http://www.google.com' },
-  { id: 13, name: 'Google 13', url: 'http://www.google.com' },
-  { id: 14, name: 'Google 14', url: 'http://www.google.com' },
-  { id: 15, name: 'Google 15', url: 'http://www.google.com' },
-  { id: 16, name: 'Google 16', url: 'http://www.google.com' },
-  { id: 17, name: 'Google 17', url: 'http://www.google.com' },
-  { id: 18, name: 'Google 18', url: 'http://www.google.com' },
-  { id: 19, name: 'Google 19', url: 'http://www.google.com' },
-  { id: 20, name: 'Google 20', url: 'http://www.google.com' }
-];
