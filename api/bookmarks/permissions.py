29b7c0ebbaa5ae04b5909d93e554355ff49d7427
Fellipe Castro <contact@fellipecastro.com>
2017-08-24 14:31:46 -0300

    Correct flake8

--- api/bookmarks/permissions.py
+++ api/bookmarks/permissions.py
@@ -5,6 +5,7 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user or request.user.is_staff
 
+
 class IsPostMethodOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return view.request.method == 'POST' or request.user.is_staff
