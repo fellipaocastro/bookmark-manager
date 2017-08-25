04acde58f9e7da778e3da95d6634496df23ef6b5
Fellipe Castro <contact@fellipecastro.com>
2017-08-25 14:46:32 -0300

    Improve frontend

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -7,7 +7,6 @@ import { UserService } from './../../services/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent implements OnInit {
