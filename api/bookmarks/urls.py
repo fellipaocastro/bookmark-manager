4b67a4346e65aeaa68efaf0eb6eaa9fa0d2e5d95
Fellipe Castro <contact@fellipecastro.com>
2017-08-25 12:25:10 -0300

    Update

--- api/bookmarks/urls.py
+++ api/bookmarks/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^bookmarks/(?P<pk>[0-9]+)/$', views.BookmarkDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
